d97db8d23dfda04eee79e4162a4f1e346a63375f
pnby <ryda1111111@gmail.com>
2024-07-28 01:17:16 +0300

    minor changes: added curr time print

--- app/api/scheduler.py
+++ app/api/scheduler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import final
 
 import schedule
@@ -11,9 +12,14 @@ from app.utils.utils import singleton
 class Scheduler:
     def __init__(self):
         self.jobs = set()
+        self.add_interval_task(10, lambda p: print(datetime.now()))
 
     def add_tasks(self, time_str, task):
         job = schedule.every().day.at(time_str).do(task)
+        self.jobs.add(job)
+
+    def add_interval_task(self, interval_seconds, task):
+        job = schedule.every(interval_seconds).seconds.do(task)
         self.jobs.add(job)
 
     @staticmethod
