cd7e3e2c0a6271b8f5038e263e055aa0636f1f72
pnby <ryda1111111@gmail.com>
2024-07-28 01:19:51 +0300

    fix: unfilled argument

--- app/api/scheduler.py
+++ app/api/scheduler.py
@@ -12,7 +12,7 @@ from app.utils.utils import singleton
 class Scheduler:
     def __init__(self):
         self.jobs = set()
-        self.add_interval_task(10, lambda p: print(datetime.now()))
+        self.add_interval_task(10, self.print_current_time())
 
     def add_tasks(self, time_str, task):
         job = schedule.every().day.at(time_str).do(task)
@@ -21,6 +21,10 @@ class Scheduler:
     def add_interval_task(self, interval_seconds, task):
         job = schedule.every(interval_seconds).seconds.do(task)
         self.jobs.add(job)
+
+    @staticmethod
+    def print_current_time():
+        print("Current time:", datetime.now())
 
     @staticmethod
     def run():
